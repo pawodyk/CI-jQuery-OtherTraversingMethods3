93ea811a320c3ebb3fc9f292d80db09479c02833
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-01-24 22:54:23 +0000

    completed challenge

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
     // put your code here
-    
+    $(".theButton").click(function() {
+        var color = $(this).css("background-color");
+        $(".superButton").text(color);
+    });
+
+    $(".superButton").click(function() {
+        $(this).text("Reset");
+    });
 });
